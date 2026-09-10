2c768821f67a37f504b80f26f4c241b22ab7ec28
AlejandraCZ06 <alejandra.lea.2023@gmail.com>
2026-09-10 12:10:06 -0500

    cambio

--- 5-ARM-Boards/NUCLEO-64/4-custom-hal/main.c
+++ 5-ARM-Boards/NUCLEO-64/4-custom-hal/main.c
@@ -17,10 +17,12 @@ typedef struct {
 
 #define GPIOA_BASE   (AHB1_BASE + 0x0000U)
 #define GPIOB_BASE   (AHB1_BASE + 0x0400U)
+#define GPIOC_BASE   (AHB1_BASE + 0x0800U)
 #define RCC_BASE     (AHB1_BASE + 0x3800U)
 
 #define GPIOA ((GPIO_TypeDef *) GPIOA_BASE)
 #define GPIOB ((GPIO_TypeDef *) GPIOB_BASE)
+#define GPIOC ((GPIO_TypeDef *) GPIOC_BASE)
 
 #define RCC_AHB1ENR ((volatile uint32_t *)(RCC_BASE + 0x30U))
 
@@ -29,6 +31,17 @@ void configurar_salida(GPIO_TypeDef *puerto, uint8_t pin)
 {
     puerto->MODER &= ~(3U << (pin * 2));
     puerto->MODER |=  (1U << (pin * 2));
+}
+
+
+void configurar_entrada_pullup(GPIO_TypeDef *puerto, uint8_t pin)
+{
+    // Entrada
+    puerto->MODER &= ~(3U << (pin * 2));
+
+    // Pull-up
+    puerto->PUPDR &= ~(3U << (pin * 2));
+    puerto->PUPDR |=  (1U << (pin * 2));
 }
 
 
@@ -41,69 +54,93 @@ void escribir(GPIO_TypeDef *puerto, uint8_t pin, uint8_t valor)
 }
 
 
+uint8_t leer_switch(uint8_t pin)
+{
+    // Switch activado = 0
+    // Switch apagado = 1
+
+    if ((GPIOC->IDR & (1U << pin)) == 0)
+        return 1;
+
+    return 0;
+}
+
+
 int main(void)
 {
-    // Activar relojes GPIOA y GPIOB
+    // Activar GPIOA, GPIOB y GPIOC
     *RCC_AHB1ENR |= (1U << 0);
     *RCC_AHB1ENR |= (1U << 1);
+    *RCC_AHB1ENR |= (1U << 2);
 
-    // -------------------------
+
+    // =========================
+    // SWITCHES PC0-PC7
+    // =========================
+
+    configurar_entrada_pullup(GPIOC, 0);
+    configurar_entrada_pullup(GPIOC, 1);
+    configurar_entrada_pullup(GPIOC, 2);
+    configurar_entrada_pullup(GPIOC, 3);
+    configurar_entrada_pullup(GPIOC, 4);
+    configurar_entrada_pullup(GPIOC, 5);
+    configurar_entrada_pullup(GPIOC, 6);
+    configurar_entrada_pullup(GPIOC, 7);
+
+
+    // =========================
     // SEGMENTOS
-    // -------------------------
+    // =========================
 
-    configurar_salida(GPIOA, 8);   // a
-    configurar_salida(GPIOA, 9);   // b
-    configurar_salida(GPIOA, 10);  // c
+    // A = PA8
+    configurar_salida(GPIOA, 8);
 
-    configurar_salida(GPIOB, 3);   // d
-    configurar_salida(GPIOB, 4);   // e
-    configurar_salida(GPIOB, 5);   // f
-    configurar_salida(GPIOB, 6);   // g
+    // B = PA9
+    configurar_salida(GPIOA, 9);
 
-    // -------------------------
-    // DIGITOS
-    // -------------------------
+    // C = PA10
+    configurar_salida(GPIOA, 10);
 
-    configurar_salida(GPIOB, 9);   // Digito 1
-    configurar_salida(GPIOB, 10);  // Digito 2
-    configurar_salida(GPIOA, 6);   // Digito 3
-    configurar_salida(GPIOA, 7);   // Digito 4
+    // D = PB3
+    configurar_salida(GPIOB, 3);
 
-    // -------------------------
-    // CÁTODO COMÚN
-    //
-    // Segmento:
-    // 1 = encendido
-    // 0 = apagado
-    //
-    // Dígito:
-    // 0 = encendido
-    // 1 = apagado
-    // -------------------------
+    // E = PB4
+    configurar_salida(GPIOB, 4);
 
-    // Apagar todos los displays
-    escribir(GPIOB, 9, 1);
-    escribir(GPIOB, 10, 1);
-    escribir(GPIOA, 6, 1);
-    escribir(GPIOA, 7, 1);
+    // F = PB5
+    configurar_salida(GPIOB, 5);
 
-    // Mostrar 8
-    // a,b,c,d,e,f,g = 1
+    // G = PB6
+    configurar_salida(GPIOB, 6);
 
-    escribir(GPIOA, 8, 1);   // a
-    escribir(GPIOA, 9, 1);   // b
-    escribir(GPIOA, 10, 1);  // c
+    // DP = PB8
+    configurar_salida(GPIOB, 8);
 
-    escribir(GPIOB, 3, 1);   // d
-    escribir(GPIOB, 4, 1);   // e
-    escribir(GPIOB, 5, 1);   // f
-    escribir(GPIOB, 6, 1);   // g
 
-    // Encender SOLO el primer display
-    escribir(GPIOB, 9, 0);
-
-    // Mantenerlo encendido
     while (1)
     {
+        // PC0 -> A
+        escribir(GPIOA, 8, leer_switch(0));
+
+        // PC1 -> B
+        escribir(GPIOA, 9, leer_switch(1));
+
+        // PC2 -> C
+        escribir(GPIOA, 10, leer_switch(2));
+
+        // PC3 -> D
+        escribir(GPIOB, 3, leer_switch(3));
+
+        // PC4 -> E
+        escribir(GPIOB, 4, leer_switch(4));
+
+        // PC5 -> F
+        escribir(GPIOB, 5, leer_switch(5));
+
+        // PC6 -> G
+        escribir(GPIOB, 6, leer_switch(6));
+
+        // PC7 -> DP
+        escribir(GPIOB, 8, leer_switch(7));
     }
 }
