04cfb0b96208795419a8f44b93f1bb9270195275
AlejandraCZ06 <alejandra.lea.2023@gmail.com>
2026-09-14 15:55:30 -0500

    cambio

--- 5-ARM-Boards/NUCLEO-64/4-custom-hal/main.c
+++ 5-ARM-Boards/NUCLEO-64/4-custom-hal/main.c
@@ -1,20 +1,80 @@
 #include "gpio_config.h"
 #include "stdint.h"
 
+
+/* =========================================================
+   TABLERO
+
+   0 = casilla libre
+   1 = jugador humano
+   2 = computadora
+
+   Posiciones:
+
+        1 | 2 | 3
+       ---+---+---
+        4 | 5 | 6
+       ---+---+---
+        7 | 8 | 9
+   ========================================================= */
 uint8_t tablero[9] = {0};
+
+
+/* =========================================================
+   GANADOR
+
+   0 = nadie
+   1 = humano
+   2 = computadora
+   ========================================================= */
 uint8_t ganador_final = 0;
+
+
+/* =========================================================
+   LINEA GANADORA
+
+   Guarda las 3 casillas que hicieron ganar.
+
+   Ejemplo:
+
+   {0,1,2} = primera fila
+   {0,4,8} = diagonal
+   ========================================================= */
+uint8_t linea_ganadora[3] = {0, 0, 0};
+
+
+/* =========================================================
+   REGISTROS DWT PARA NUMERO PSEUDOALEATORIO
+   ========================================================= */
 
 #define DEMCR      (*(volatile uint32_t *)0xE000EDFC)
 #define DWT_CTRL   (*(volatile uint32_t *)0xE0001000)
 #define DWT_CYCCNT (*(volatile uint32_t *)0xE0001004)
 
+
+/* =========================================================
+   INICIAR CONTADOR DWT
+   ========================================================= */
 void iniciar_aleatorio(void)
 {
+    /* Habilitar trace */
     DEMCR |= (1 << 24);
+
+    /* Reiniciar contador */
     DWT_CYCCNT = 0;
+
+    /* Habilitar contador */
     DWT_CTRL |= 1;
 }
 
+
+/* =========================================================
+   NUMERO PSEUDOALEATORIO
+
+   Devuelve un numero entre:
+
+   0 y max-1
+   ========================================================= */
 uint8_t numero_aleatorio(uint8_t max)
 {
     if (max == 0)
@@ -23,21 +83,37 @@ uint8_t numero_aleatorio(uint8_t max)
     return DWT_CYCCNT % max;
 }
 
+
+/* =========================================================
+   COMPROBAR GANADOR
+
+   jugador = 1 -> humano
+   jugador = 2 -> computadora
+
+   Si gana:
+   guarda las 3 posiciones ganadoras.
+   ========================================================= */
 uint8_t ganador(uint8_t jugador)
 {
     uint8_t c[8][3] =
     {
-        {0,1,2},
-        {3,4,5},
-        {6,7,8},
-        {0,3,6},
-        {1,4,7},
-        {2,5,8},
-        {0,4,8},
-        {2,4,6}
+        /* Filas */
+        {0, 1, 2},
+        {3, 4, 5},
+        {6, 7, 8},
+
+        /* Columnas */
+        {0, 3, 6},
+        {1, 4, 7},
+        {2, 5, 8},
+
+        /* Diagonales */
+        {0, 4, 8},
+        {2, 4, 6}
     };
 
     uint8_t i;
+
 
     for (i = 0; i < 8; i++)
     {
@@ -45,6 +121,14 @@ uint8_t ganador(uint8_t jugador)
             tablero[c[i][1]] == jugador &&
             tablero[c[i][2]] == jugador)
         {
+            /*
+             * Guardar exactamente las 3
+             * casillas de la linea ganadora.
+             */
+            linea_ganadora[0] = c[i][0];
+            linea_ganadora[1] = c[i][1];
+            linea_ganadora[2] = c[i][2];
+
             return 1;
         }
     }
@@ -52,6 +136,10 @@ uint8_t ganador(uint8_t jugador)
     return 0;
 }
 
+
+/* =========================================================
+   COMPROBAR SI EL TABLERO ESTA LLENO
+   ========================================================= */
 uint8_t tablero_lleno(void)
 {
     uint8_t i;
@@ -65,20 +153,37 @@ uint8_t tablero_lleno(void)
     return 1;
 }
 
+
+/* =========================================================
+   LIMPIAR TABLERO PARA NUEVA PARTIDA
+   ========================================================= */
 void limpiar_tablero(void)
 {
     uint8_t i;
 
     ganador_final = 0;
 
+    linea_ganadora[0] = 0;
+    linea_ganadora[1] = 0;
+    linea_ganadora[2] = 0;
+
+
     for (i = 0; i < 9; i++)
     {
         tablero[i] = 0;
+
+        /* Apagar rojo */
         led_rojo(i + 1, 0);
+
+        /* Apagar azul */
         led_azul(i + 1, 0);
     }
 }
 
+
+/* =========================================================
+   ESPERAR A QUE SE SUELTE UN BOTON
+   ========================================================= */
 void esperar_soltura(uint8_t boton)
 {
     while (boton_presionado(boton))
@@ -86,6 +191,10 @@ void esperar_soltura(uint8_t boton)
     }
 }
 
+
+/* =========================================================
+   ESPERAR A QUE SE PRESIONE B1
+   ========================================================= */
 void esperar_B1(void)
 {
     while (!boton_start())
@@ -93,6 +202,10 @@ void esperar_B1(void)
     }
 }
 
+
+/* =========================================================
+   ESPERAR A QUE SE SUELTE B1
+   ========================================================= */
 void soltar_B1(void)
 {
     while (boton_start())
@@ -100,12 +213,24 @@ void soltar_B1(void)
     }
 }
 
+
+/* =========================================================
+   BUSCAR UNA CASILLA LIBRE ALEATORIAMENTE
+
+   Devuelve:
+   0..8 = casilla libre
+   9    = no hay casillas
+   ========================================================= */
 uint8_t casilla_libre(void)
 {
     uint8_t libres[9];
     uint8_t cantidad = 0;
     uint8_t i;
 
+
+    /*
+     * Guardar todas las casillas libres.
+     */
     for (i = 0; i < 9; i++)
     {
         if (tablero[i] == 0)
@@ -115,69 +240,133 @@ uint8_t casilla_libre(void)
         }
     }
 
+
+    /*
+     * No hay casillas libres.
+     */
     if (cantidad == 0)
         return 9;
 
+
+    /*
+     * Elegir aleatoriamente una
+     * de las casillas libres.
+     */
     return libres[numero_aleatorio(cantidad)];
 }
 
+
+/* =========================================================
+   JUGADA DE LA COMPUTADORA
+   ========================================================= */
 void jugada_computadora(void)
 {
     uint8_t casilla;
 
+
     casilla = casilla_libre();
+
 
     if (casilla < 9)
     {
+        /*
+         * Marcar casilla como computadora.
+         */
         tablero[casilla] = 2;
+
+
+        /*
+         * Encender LED rojo.
+         */
         led_rojo(casilla + 1, 1);
     }
 }
 
+
+/* =========================================================
+   JUEGO
+   ========================================================= */
 void jugar(void)
 {
     uint8_t i;
 
+
     while (1)
     {
+        /*
+         * Revisar los 9 botones.
+         */
         for (i = 0; i < 9; i++)
         {
             if (boton_presionado(i + 1))
             {
+                /*
+                 * Si la casilla ya esta ocupada,
+                 * ignorar el boton.
+                 */
                 if (tablero[i] != 0)
                 {
                     esperar_soltura(i + 1);
                     continue;
                 }
 
-                /* Jugada humana */
+
+                /* =========================================
+                   JUGADA DEL HUMANO
+                   ========================================= */
+
                 tablero[i] = 1;
+
+                /*
+                 * Encender LED azul.
+                 */
                 led_azul(i + 1, 1);
 
+
+                /*
+                 * Esperar a que el usuario suelte
+                 * el boton.
+                 */
                 esperar_soltura(i + 1);
 
-                /* Gana humano */
+
+                /*
+                 * Comprobar si gano el humano.
+                 */
                 if (ganador(1))
                 {
                     ganador_final = 1;
                     return;
                 }
 
-                /* Empate */
+
+                /*
+                 * Comprobar empate.
+                 */
                 if (tablero_lleno())
                     return;
 
-                /* Jugada computadora */
+
+                /* =========================================
+                   JUGADA DE LA COMPUTADORA
+                   ========================================= */
+
                 jugada_computadora();
 
-                /* Gana computadora */
+
+                /*
+                 * Comprobar si gano la computadora.
+                 */
                 if (ganador(2))
                 {
                     ganador_final = 2;
                     return;
                 }
 
-                /* Empate */
+
+                /*
+                 * Comprobar empate.
+                 */
                 if (tablero_lleno())
                     return;
             }
@@ -185,101 +374,224 @@ void jugar(void)
     }
 }
 
+
+/* =========================================================
+   TITILAR SOLAMENTE LOS 3 LEDS GANADORES
+
+   Si gana humano:
+       titilan los 3 LEDS AZULES ganadores.
+
+   Si gana computadora:
+       titilan los 3 LEDS ROJOS ganadores.
+
+   Los otros 6 LEDs quedan apagados.
+   ========================================================= */
 void titilar_ganador(void)
 {
     uint8_t i;
     uint8_t j;
+
     volatile uint32_t d;
 
+
+    /*
+     * Si no hay ganador, no hacer nada.
+     *
+     * Esto ocurre en caso de empate.
+     */
     if (ganador_final == 0)
         return;
 
-    /* Apagar LEDs del perdedor */
+
+    /* =====================================================
+       APAGAR TODOS LOS LEDS
+       ===================================================== */
+
     for (i = 0; i < 9; i++)
     {
-        if (tablero[i] != ganador_final)
-        {
-            led_rojo(i + 1, 0);
-            led_azul(i + 1, 0);
-        }
+        led_rojo(i + 1, 0);
+        led_azul(i + 1, 0);
     }
 
-    /* Titilar ganador 3 veces */
+
+    /* =====================================================
+       TITILAR 3 VECES
+       ===================================================== */
+
     for (j = 0; j < 3; j++)
     {
-        /* Apagar ganador */
-        for (i = 0; i < 9; i++)
+        /* ================================================
+           APAGAR LOS 3 GANADORES
+           ================================================ */
+
+        for (i = 0; i < 3; i++)
         {
-            if (tablero[i] == ganador_final)
+            if (ganador_final == 1)
             {
-                if (ganador_final == 1)
-                    led_azul(i + 1, 0);
-                else
-                    led_rojo(i + 1, 0);
+                /*
+                 * Gano humano -> azul
+                 */
+                led_azul(linea_ganadora[i] + 1, 0);
+            }
+            else
+            {
+                /*
+                 * Gano computadora -> rojo
+                 */
+                led_rojo(linea_ganadora[i] + 1, 0);
             }
         }
 
+
+        /*
+         * Retardo
+         */
         for (d = 0; d < 500000; d++)
         {
         }
 
-        /* Encender ganador */
-        for (i = 0; i < 9; i++)
+
+        /* ================================================
+           ENCENDER LOS 3 GANADORES
+           ================================================ */
+
+        for (i = 0; i < 3; i++)
         {
-            if (tablero[i] == ganador_final)
+            if (ganador_final == 1)
             {
-                if (ganador_final == 1)
-                    led_azul(i + 1, 1);
-                else
-                    led_rojo(i + 1, 1);
+                /*
+                 * Gano humano -> azul
+                 */
+                led_azul(linea_ganadora[i] + 1, 1);
+            }
+            else
+            {
+                /*
+                 * Gano computadora -> rojo
+                 */
+                led_rojo(linea_ganadora[i] + 1, 1);
             }
         }
 
+
+        /*
+         * Retardo
+         */
         for (d = 0; d < 500000; d++)
         {
         }
     }
+
+
+    /*
+     * Al terminar los 3 destellos,
+     * dejar los 3 ganadores encendidos.
+     *
+     * Los otros 6 permanecen apagados.
+     */
 }
 
+
+/* =========================================================
+   MAIN
+   ========================================================= */
 int main(void)
 {
     uint8_t turno;
 
+
+    /* Configurar GPIO */
     GPIO_Config();
+
+
+    /* Iniciar generador pseudoaleatorio */
     iniciar_aleatorio();
+
 
     while (1)
     {
-        /* B1 inicia la partida */
+        /* =================================================
+           ESPERAR B1
+
+           B1 inicia una nueva partida.
+           ================================================= */
         esperar_B1();
 
-        /* Obtener aleatorio mientras B1 esta presionado */
+
+        /* =================================================
+           DECIDIR ALEATORIAMENTE QUIEN EMPIEZA
+
+           turno = 0 -> humano
+           turno = 1 -> computadora
+           ================================================= */
         turno = numero_aleatorio(2);
 
-        /* Esperar que B1 sea soltado */
+
+        /* =================================================
+           ESPERAR QUE B1 SEA SOLTADO
+           ================================================= */
         soltar_B1();
 
-        /* Nueva partida */
+
+        /* =================================================
+           LIMPIAR TABLERO
+           ================================================= */
         limpiar_tablero();
 
-        /* Computadora empieza */
+
+        /* =================================================
+           SI EMPIEZA LA COMPUTADORA
+           ================================================= */
         if (turno == 1)
         {
             jugada_computadora();
 
+
+            /*
+             * Comprobar si la computadora gano.
+             *
+             * Normalmente no puede ganar en la primera
+             * jugada, pero se mantiene la comprobacion.
+             */
             if (ganador(2))
+            {
                 ganador_final = 2;
+            }
         }
 
-        /* Jugar */
-        if (ganador_final == 0)
-            jugar();
 
-        /* Mostrar resultado */
+        /* =================================================
+           JUGAR
+
+           Si la computadora no gano inmediatamente,
+           empieza el juego normal.
+           ================================================= */
+        if (ganador_final == 0)
+        {
+            jugar();
+        }
+
+
+        /* =================================================
+           MOSTRAR RESULTADO
+
+           Si hubo ganador:
+               solo titilan los 3 ganadores.
+
+           Si hubo empate:
+               no titila nada.
+               El tablero queda lleno.
+           ================================================= */
         titilar_ganador();
 
-        /* Esperar B1 para la siguiente partida */
+
+        /*
+         * Volver al principio.
+         *
+         * El siguiente B1 comienza otra partida.
+         */
     }
+
 
     return 0;
 }
--- 5-ARM-Boards/NUCLEO-64/4-custom-hal/src/gpio_config.c
+++ 5-ARM-Boards/NUCLEO-64/4-custom-hal/src/gpio_config.c
@@ -1,21 +1,44 @@
 #include "gpio_config.h"
 
+
+/* =========================================================
+   CONFIGURAR ENTRADA CON PULL-UP
+   Se usa para los 9 botones externos.
+
+   Boton sin presionar = 1
+   Boton presionado    = 0
+   ========================================================= */
 static void configurar_entrada_pullup(volatile GPIO_TypeDef *GPIOx,
                                       uint8_t pin)
 {
     GPIOx->MODER &= ~(3 << (pin * 2));
+
     GPIOx->PUPDR &= ~(3 << (pin * 2));
     GPIOx->PUPDR |=  (1 << (pin * 2));
 }
 
+
+/* =========================================================
+   CONFIGURAR ENTRADA CON PULL-DOWN
+
+   Se usa para B1 en PC13.
+
+   B1 sin presionar = 0
+   B1 presionado    = 1
+   ========================================================= */
 static void configurar_entrada_pulldown(volatile GPIO_TypeDef *GPIOx,
                                         uint8_t pin)
 {
     GPIOx->MODER &= ~(3 << (pin * 2));
+
     GPIOx->PUPDR &= ~(3 << (pin * 2));
     GPIOx->PUPDR |=  (2 << (pin * 2));
 }
 
+
+/* =========================================================
+   CONFIGURAR SALIDA
+   ========================================================= */
 static void configurar_salida(volatile GPIO_TypeDef *GPIOx,
                               uint8_t pin)
 {
@@ -23,53 +46,136 @@ static void configurar_salida(volatile GPIO_TypeDef *GPIOx,
     GPIOx->MODER |=  (1 << (pin * 2));
 }
 
+
+/* =========================================================
+   CONFIGURACION GENERAL GPIO
+   ========================================================= */
 void GPIO_Config(void)
 {
+    /*
+     * Habilitar relojes GPIOA, GPIOB y GPIOC
+     */
     RCC->AHB1ENR |= (1 << 0);
     RCC->AHB1ENR |= (1 << 1);
     RCC->AHB1ENR |= (1 << 2);
 
+    /*
+     * Pequeño acceso para asegurar habilitacion
+     */
     volatile unsigned int dummy;
+
     dummy = RCC->AHB1ENR;
     dummy = RCC->AHB1ENR;
 
-    /* BOTONES EXTERNOS */
+
+    /* =====================================================
+       BOTONES EXTERNOS
+       ===================================================== */
+
+    /* Boton 1 -> PC0 */
     configurar_entrada_pullup(GPIOC, 0);
+
+    /* Boton 2 -> PC1 */
     configurar_entrada_pullup(GPIOC, 1);
+
+    /* Boton 3 -> PB8 */
     configurar_entrada_pullup(GPIOB, 8);
+
+    /* Boton 4 -> PB13 */
     configurar_entrada_pullup(GPIOB, 13);
+
+    /* Boton 5 -> PB14 */
     configurar_entrada_pullup(GPIOB, 14);
+
+    /* Boton 6 -> PC5 */
     configurar_entrada_pullup(GPIOC, 5);
+
+    /* Boton 7 -> PC6 */
     configurar_entrada_pullup(GPIOC, 6);
+
+    /* Boton 8 -> PC7 */
     configurar_entrada_pullup(GPIOC, 7);
+
+    /* Boton 9 -> PA10 */
     configurar_entrada_pullup(GPIOA, 10);
 
-    /* B1 - PC13 */
+
+    /* =====================================================
+       B1 DE LA NUCLEO
+       PC13
+       ===================================================== */
+
     configurar_entrada_pulldown(GPIOC, 13);
 
-    /* LEDS ROJOS */
+
+    /* =====================================================
+       LEDS ROJOS
+       ===================================================== */
+
+    /* LED rojo 1 -> PA0 */
     configurar_salida(GPIOA, 0);
+
+    /* LED rojo 2 -> PA1 */
     configurar_salida(GPIOA, 1);
+
+    /* LED rojo 3 -> PB9 */
     configurar_salida(GPIOB, 9);
+
+    /* LED rojo 4 -> PA8 */
     configurar_salida(GPIOA, 8);
+
+    /* LED rojo 5 -> PA4 */
     configurar_salida(GPIOA, 4);
+
+    /* LED rojo 6 -> PA6 */
     configurar_salida(GPIOA, 6);
+
+    /* LED rojo 7 -> PA5 */
     configurar_salida(GPIOA, 5);
+
+    /* LED rojo 8 -> PA7 */
     configurar_salida(GPIOA, 7);
+
+    /* LED rojo 9 -> PB15 */
     configurar_salida(GPIOB, 15);
 
-    /* LEDS AZULES */
+
+    /* =====================================================
+       LEDS AZULES
+       ===================================================== */
+
+    /* LED azul 1 -> PB7 */
     configurar_salida(GPIOB, 7);
+
+    /* LED azul 2 -> PB1 */
     configurar_salida(GPIOB, 1);
+
+    /* LED azul 3 -> PB2 */
     configurar_salida(GPIOB, 2);
+
+    /* LED azul 4 -> PB3 */
     configurar_salida(GPIOB, 3);
+
+    /* LED azul 5 -> PB4 */
     configurar_salida(GPIOB, 4);
+
+    /* LED azul 6 -> PB5 */
     configurar_salida(GPIOB, 5);
+
+    /* LED azul 7 -> PB6 */
     configurar_salida(GPIOB, 6);
+
+    /* LED azul 8 -> PC8 */
     configurar_salida(GPIOC, 8);
+
+    /* LED azul 9 -> PA9 */
     configurar_salida(GPIOA, 9);
 
-    /* APAGAR LEDS ROJOS */
+
+    /* =====================================================
+       APAGAR TODOS LOS LEDS ROJOS
+       ===================================================== */
+
     write_pin_state(GPIOA, 0, 0);
     write_pin_state(GPIOA, 1, 0);
     write_pin_state(GPIOB, 9, 0);
@@ -80,7 +186,11 @@ void GPIO_Config(void)
     write_pin_state(GPIOA, 7, 0);
     write_pin_state(GPIOB, 15, 0);
 
-    /* APAGAR LEDS AZULES */
+
+    /* =====================================================
+       APAGAR TODOS LOS LEDS AZULES
+       ===================================================== */
+
     write_pin_state(GPIOB, 7, 0);
     write_pin_state(GPIOB, 1, 0);
     write_pin_state(GPIOB, 2, 0);
@@ -92,11 +202,23 @@ void GPIO_Config(void)
     write_pin_state(GPIOA, 9, 0);
 }
 
-uint8_t read_pin_state(volatile GPIO_TypeDef *GPIOx, uint8_t pin)
+
+/* =========================================================
+   LEER GPIO
+   ========================================================= */
+uint8_t read_pin_state(volatile GPIO_TypeDef *GPIOx,
+                       uint8_t pin)
 {
-    return (GPIOx->IDR & (1 << pin)) ? 1 : 0;
+    if (GPIOx->IDR & (1 << pin))
+        return 1;
+    else
+        return 0;
 }
 
+
+/* =========================================================
+   ESCRIBIR GPIO
+   ========================================================= */
 void write_pin_state(volatile GPIO_TypeDef *GPIOx,
                      uint8_t pin,
                      uint8_t state)
@@ -107,58 +229,155 @@ void write_pin_state(volatile GPIO_TypeDef *GPIOx,
         GPIOx->ODR &= ~(1 << pin);
 }
 
+
+/* =========================================================
+   LEER LOS 9 BOTONES DEL TABLERO
+
+   1 = presionado
+   0 = no presionado
+   ========================================================= */
 uint8_t boton_presionado(uint8_t boton)
 {
     switch (boton)
     {
-        case 1: return read_pin_state(GPIOC, 0) == 0;
-        case 2: return read_pin_state(GPIOC, 1) == 0;
-        case 3: return read_pin_state(GPIOB, 8) == 0;
-        case 4: return read_pin_state(GPIOB, 13) == 0;
-        case 5: return read_pin_state(GPIOB, 14) == 0;
-        case 6: return read_pin_state(GPIOC, 5) == 0;
-        case 7: return read_pin_state(GPIOC, 6) == 0;
-        case 8: return read_pin_state(GPIOC, 7) == 0;
-        case 9: return read_pin_state(GPIOA, 10) == 0;
-        default: return 0;
+        case 1:
+            return read_pin_state(GPIOC, 0) == 0;
+
+        case 2:
+            return read_pin_state(GPIOC, 1) == 0;
+
+        case 3:
+            return read_pin_state(GPIOB, 8) == 0;
+
+        case 4:
+            return read_pin_state(GPIOB, 13) == 0;
+
+        case 5:
+            return read_pin_state(GPIOB, 14) == 0;
+
+        case 6:
+            return read_pin_state(GPIOC, 5) == 0;
+
+        case 7:
+            return read_pin_state(GPIOC, 6) == 0;
+
+        case 8:
+            return read_pin_state(GPIOC, 7) == 0;
+
+        case 9:
+            return read_pin_state(GPIOA, 10) == 0;
+
+        default:
+            return 0;
     }
 }
 
+
+/* =========================================================
+   LEER B1
+
+   B1 presionado = 1
+   B1 no presionado = 0
+   ========================================================= */
 uint8_t boton_start(void)
 {
     return read_pin_state(GPIOC, 13) == 1;
 }
 
+
+/* =========================================================
+   CONTROL LED ROJO
+   ========================================================= */
 void led_rojo(uint8_t led, uint8_t estado)
 {
     switch (led)
     {
-        case 1: write_pin_state(GPIOA, 0, estado); break;
-        case 2: write_pin_state(GPIOA, 1, estado); break;
-        case 3: write_pin_state(GPIOB, 9, estado); break;
-        case 4: write_pin_state(GPIOA, 8, estado); break;
-        case 5: write_pin_state(GPIOA, 4, estado); break;
-        case 6: write_pin_state(GPIOA, 6, estado); break;
-        case 7: write_pin_state(GPIOA, 5, estado); break;
-        case 8: write_pin_state(GPIOA, 7, estado); break;
-        case 9: write_pin_state(GPIOB, 15, estado); break;
-        default: break;
+        case 1:
+            write_pin_state(GPIOA, 0, estado);
+            break;
+
+        case 2:
+            write_pin_state(GPIOA, 1, estado);
+            break;
+
+        case 3:
+            write_pin_state(GPIOB, 9, estado);
+            break;
+
+        case 4:
+            write_pin_state(GPIOA, 8, estado);
+            break;
+
+        case 5:
+            write_pin_state(GPIOA, 4, estado);
+            break;
+
+        case 6:
+            write_pin_state(GPIOA, 6, estado);
+            break;
+
+        case 7:
+            write_pin_state(GPIOA, 5, estado);
+            break;
+
+        case 8:
+            write_pin_state(GPIOA, 7, estado);
+            break;
+
+        case 9:
+            write_pin_state(GPIOB, 15, estado);
+            break;
+
+        default:
+            break;
     }
 }
 
+
+/* =========================================================
+   CONTROL LED AZUL
+   ========================================================= */
 void led_azul(uint8_t led, uint8_t estado)
 {
     switch (led)
     {
-        case 1: write_pin_state(GPIOB, 7, estado); break;
-        case 2: write_pin_state(GPIOB, 1, estado); break;
-        case 3: write_pin_state(GPIOB, 2, estado); break;
-        case 4: write_pin_state(GPIOB, 3, estado); break;
-        case 5: write_pin_state(GPIOB, 4, estado); break;
-        case 6: write_pin_state(GPIOB, 5, estado); break;
-        case 7: write_pin_state(GPIOB, 6, estado); break;
-        case 8: write_pin_state(GPIOC, 8, estado); break;
-        case 9: write_pin_state(GPIOA, 9, estado); break;
-        default: break;
+        case 1:
+            write_pin_state(GPIOB, 7, estado);
+            break;
+
+        case 2:
+            write_pin_state(GPIOB, 1, estado);
+            break;
+
+        case 3:
+            write_pin_state(GPIOB, 2, estado);
+            break;
+
+        case 4:
+            write_pin_state(GPIOB, 3, estado);
+            break;
+
+        case 5:
+            write_pin_state(GPIOB, 4, estado);
+            break;
+
+        case 6:
+            write_pin_state(GPIOB, 5, estado);
+            break;
+
+        case 7:
+            write_pin_state(GPIOB, 6, estado);
+            break;
+
+        case 8:
+            write_pin_state(GPIOC, 8, estado);
+            break;
+
+        case 9:
+            write_pin_state(GPIOA, 9, estado);
+            break;
+
+        default:
+            break;
     }
 }
