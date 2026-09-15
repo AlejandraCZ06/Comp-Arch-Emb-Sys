d78d149a1556e5db82fbe29850ed355f875c0122
AlejandraCZ06 <alejandra.lea.2023@gmail.com>
2026-09-15 13:34:05 -0500

    cambios

--- 5-ARM-Boards/NUCLEO-64/4-custom-hal/main.c
+++ 5-ARM-Boards/NUCLEO-64/4-custom-hal/main.c
@@ -1,27 +1,62 @@
 #include "gpio_config.h"
 
+volatile uint8_t hundreds;
+volatile uint8_t tens;
+volatile uint8_t units;
+
 int main(void)
 {
-    uint16_t anterior = 0;
-    uint16_t actual = 1;
-    uint16_t siguiente;
+    uint8_t entrada;
 
-    GPIO_Config();
+    *RCC_AHB1ENR |= (1U << 0);
+    *RCC_AHB1ENR |= (1U << 1);
+    *RCC_AHB1ENR |= (1U << 2);
+
+    configurar_entrada_pullup(GPIOC, 0);
+    configurar_entrada_pullup(GPIOC, 1);
+    configurar_entrada_pullup(GPIOC, 2);
+    configurar_entrada_pullup(GPIOC, 3);
+    configurar_entrada_pullup(GPIOC, 4);
+    configurar_entrada_pullup(GPIOC, 5);
+    configurar_entrada_pullup(GPIOC, 6);
+    configurar_entrada_pullup(GPIOC, 7);
+
+    configurar_salida(GPIOA, 8);
+    configurar_salida(GPIOA, 9);
+    configurar_salida(GPIOA, 10);
+
+    configurar_salida(GPIOB, 3);
+    configurar_salida(GPIOB, 4);
+    configurar_salida(GPIOB, 5);
+    configurar_salida(GPIOB, 6);
+    configurar_salida(GPIOB, 8);
+    configurar_salida(GPIOB, 9);
+    configurar_salida(GPIOB, 10);
+
+    configurar_salida(GPIOA, 6);
 
     while (1)
     {
-        mostrar_3_digitos_250ms(actual);
+        entrada = leer_dip();
 
-        siguiente = anterior + actual;
+        hundreds = entrada / 100;
+        tens = (entrada / 10) % 10;
+        units = entrada % 10;
 
-        anterior = actual;
-        actual = siguiente;
+        apagar_digitos();
+        mostrar_numero(hundreds);
+        escribir(GPIOB, 9, 0);
+        retardo(1000);
 
-        if (actual > 987)
-        {
-            anterior = 0;
-            actual = 1;
-        }
+        apagar_digitos();
+        mostrar_numero(tens);
+        escribir(GPIOB, 10, 0);
+        retardo(1000);
+
+        apagar_digitos();
+        mostrar_numero(units);
+        escribir(GPIOA, 6, 0);
+        retardo(1000);
     }
 
     HALT();
